6086bac2d98d10e61048c5bfc651db0185494ebb
Fabiana Cervini <fabiana.cervini@outlook.com>
2023-08-15 18:49:20 -0300

    historial js

--- js/historial.js
+++ js/historial.js
@@ -1,34 +1,45 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Obtener el elemento de la lista de préstamos
-    const listaPrestamos = document.querySelector('.lista-prestamos');
-  
-    // Cargar historial de préstamos almacenados en LocalStorage al cargar la página
-    const historialPrestamos = JSON.parse(localStorage.getItem('historialPrestamos'));
-    if (historialPrestamos) {
-      // Agregar los préstamos al historial
-      historialPrestamos.forEach((prestamo) => {
-        agregarPrestamoAlHistorial(prestamo);
+  const listaPrestamos = document.querySelector('.lista-prestamos');
+
+  // Función para cargar y mostrar el historial de préstamos
+  async function cargarHistorial() {
+    // Limpiar la lista de préstamos
+    listaPrestamos.innerHTML = '';
+
+    try {
+      // Obtener el historial de préstamos desde el servidor utilizando fetch
+      const response = await fetch('URL_DEL_ENDPOINT_DEL_HISTORIAL');
+      const historial = await response.json();
+
+      // Recorrer el historial y crear elementos de lista para cada préstamo
+      historial.forEach((prestamo, index) => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `Préstamo ${index + 1}: Monto: $${prestamo.montoPrestamo}, Plazo: ${prestamo.plazoPrestamo} meses, Tasa: ${prestamo.tasaInteres * 100}%, Cuota: $${prestamo.pagoMensual}`;
+        listaPrestamos.appendChild(listItem);
       });
+    } catch (error) {
+      console.error('Error al cargar el historial:', error);
     }
-  
-    // Función para agregar un préstamo al historial
-    function agregarPrestamoAlHistorial(prestamo) {
-      const prestamoElement = document.createElement('li');
-      prestamoElement.classList.add('prestamo');
-  
-      const prestamoInfo = document.createElement('div');
-      prestamoInfo.classList.add('prestamo-info');
-  
-      const titulo = document.createElement('h3');
-      titulo.textContent = `Monto: $${prestamo.montoPrestamo.toFixed(2)} - Cuota: $${prestamo.pagoMensual.toFixed(2)}`;
-  
-      const detalles = document.createElement('span');
-      detalles.textContent = `Plazo: ${prestamo.plazoPrestamo} meses - Tasa: ${prestamo.tasaInteres * 100}%`;
-  
-      prestamoInfo.appendChild(titulo);
-      prestamoInfo.appendChild(detalles);
-      prestamoElement.appendChild(prestamoInfo);
-  
-      listaPrestamos.appendChild(prestamoElement);
-    }
+  }
+
+  // Cargar y mostrar el historial al cargar la página
+  cargarHistorial();
+});
+
+ocument.addEventListener('DOMContentLoaded', function () {
+  const listaPrestamos = document.querySelector('.lista-prestamos');
+
+  // Simulación de datos de historial (comenta esta sección si el fetch funciona)
+  const datosPrestamoSimulados = [
+    { montoPrestamo: 1000, plazoPrestamo: 12, tasaInteres: 0.05, pagoMensual: 100 },
+    { montoPrestamo: 2000, plazoPrestamo: 24, tasaInteres: 0.08, pagoMensual: 150 },
+    // Agrega más datos simulados si es necesario
+  ];
+
+  // Recorrer el historial y crear elementos de lista para cada préstamo
+  datosPrestamoSimulados.forEach((prestamo, index) => {
+    const listItem = document.createElement('li');
+    listItem.textContent = `Préstamo ${index + 1}: Monto: $${prestamo.montoPrestamo}, Plazo: ${prestamo.plazoPrestamo} meses, Tasa: ${prestamo.tasaInteres * 100}%, Cuota: $${prestamo.pagoMensual}`;
+    listaPrestamos.appendChild(listItem);
   });
+});

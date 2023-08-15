e8d3a01e282d4877a95272febb2ee9d8b60d62c6
Fabiana Cervini <fabiana.cervini@outlook.com>
2023-08-15 18:51:20 -0300

    historial js

--- js/historial.js
+++ js/historial.js
@@ -25,21 +25,3 @@ document.addEventListener('DOMContentLoaded', function () {
   // Cargar y mostrar el historial al cargar la página
   cargarHistorial();
 });
-
-ocument.addEventListener('DOMContentLoaded', function () {
-  const listaPrestamos = document.querySelector('.lista-prestamos');
-
-  // Simulación de datos de historial (comenta esta sección si el fetch funciona)
-  const datosPrestamoSimulados = [
-    { montoPrestamo: 1000, plazoPrestamo: 12, tasaInteres: 0.05, pagoMensual: 100 },
-    { montoPrestamo: 2000, plazoPrestamo: 24, tasaInteres: 0.08, pagoMensual: 150 },
-    // Agrega más datos simulados si es necesario
-  ];
-
-  // Recorrer el historial y crear elementos de lista para cada préstamo
-  datosPrestamoSimulados.forEach((prestamo, index) => {
-    const listItem = document.createElement('li');
-    listItem.textContent = `Préstamo ${index + 1}: Monto: $${prestamo.montoPrestamo}, Plazo: ${prestamo.plazoPrestamo} meses, Tasa: ${prestamo.tasaInteres * 100}%, Cuota: $${prestamo.pagoMensual}`;
-    listaPrestamos.appendChild(listItem);
-  });
-});

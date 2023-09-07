062ffdb20cda3c142eb2b1dd91fb04feee790471
Fabiana Cervini <fabiana.cervini@outlook.com>
2023-09-07 00:11:53 -0300

    finalizando subida API

--- js/cotizaciones.js
+++ js/cotizaciones.js
@@ -1,14 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
   const listaProductos = document.getElementById('productos');
 
-  // Utiliza Fetch para obtener los datos del archivo JSON
+  // Utiliza Fetch para traer los datos del archivo JSON
   fetch('../json/datos.json')
       .then(response => response.json())
       .then(data => {
           // Itera a través de los datos y crea elementos HTML para mostrarlos
           data.forEach(producto => {
               const li = document.createElement('li');
-              li.textContent = `${producto.nombre}: $${producto.precio}`;
+              li.classList.add('producto'); // Agrega la clase 'producto' al elemento <li>
+              li.innerHTML = `
+                <div class="nombre">${producto.nombre}</div>
+                <div class="monto">Monto: $${producto.monto}</div>
+                <div class="plazo">Plazo: ${producto.plazo} meses</div>
+                <div class="cuota">Cuota: $${producto.cuota || 'N/A'}</div>
+              `;
               listaProductos.appendChild(li);
           });
       })
